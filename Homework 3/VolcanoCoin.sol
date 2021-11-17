ac66ab39b2912f7cff43a8db86e9bea60aeb548c
Alex-March <o.marchenko@queensu.ca>
2021-11-17 15:19:26 +0200

    homework upload update

--- Homework 3/VolcanoCoin.sol
+++ Homework 3/VolcanoCoin.sol
@@ -1,43 +1,39 @@
 // SPDX-License-Identifier:UNLICENSED
 pragma solidity ^0.8.0;
 
-contract VolanoCoin{
+contract VolanoCoin {
+    //Create a variable to hold the total supply of 10000.
+    uint256 public totalSupply;
 
-//Create a variable to hold the total supply of 10000.
-uint supply;
+    //Initialise an address variable called owner
+    address owner;
 
-//Initialise an address variable called owner
-address owner;
-
-//create a modifier which only allows an owner to execute certain functions
-modifier onlyOwner()   {
-    if(msg.sender==owner){
-        _;
+    //create a modifier which only allows an owner to execute certain functions
+    modifier onlyOwner() {
+        if (msg.sender == owner) {
+            _;
+        }
     }
-}
 
-event  SupplyChange();
+    event SupplyChange(uint256);
 
-//Create a variable to hold the total supply of 10000.
-//The contract owner’s address should only be updateable in one place. Create aconstructor and within the constructor, store the owner’s address.
-constructor() {
-    supply = 10000;
-    owner = msg.sender;
-} 
+    //Create a variable to hold the total supply of 10000.
+    //Create aconstructor and within the constructor, store the owner’s address.
+    constructor() {
+        totalSupply = 10000;
+        owner = msg.sender;
+    }
 
-//Make a public function that returns the total supply.
-function totalSupply() public view returns (uint) {
-//    supply = _supply;
-    return supply;
-}
+    //Make a public function that returns the total supply.
+    function AllTotalSupply() public view returns (uint256) {
+        //    supply = _supply;
+        return totalSupply;
+    }
 
-//Make a function that can increase the total supply. Inside the function, add 1000 tokensto the current total supply.
-//Make your increase total supply function public, but add your modifier so that only theowner can execute it.
-function increaseSupply(uint _amount) public onlyOwner {
-    supply = supply + _amount;
-    emit SupplyChange();
-}
-
-
-
+    //Make a function that can increase the total supply. Inside the function, add 1000 tokensto the current total supply.
+    //Make your increase total supply function public, but add your modifier so that only theowner can execute it.
+    function increaseSupply(uint256 _amount) public onlyOwner {
+        totalSupply = totalSupply + _amount;
+        emit SupplyChange(totalSupply);
+    }
 }
--- Homework 4/VolcanoCoin2.sol
+++ Homework 4/VolcanoCoin2.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 contract VolanoCoin{
 
-uint supply;
+uint totalSupply;
 
 address owner;
 
@@ -13,7 +13,8 @@ modifier onlyOwner()   {
     }
 }
 
-event  SupplyChange(uint);
+event SupplyChange(uint);
+
 
 //Homework 4
 // 1. In order to keep track of user balances, we need to associate a user’s address with thebalance that they have.
@@ -27,13 +28,11 @@ struct Balance {
 }
 
 
-
-
 //Homework 4
 // 2. We want to allow the balance variable to be read from the contract, there are 2 ways todo this.
 // a) What are those ways ?
-// can add "view" to the function
-// can use an "event"
+// can use mappings
+// can use structs
 // b) Use one of the ways to make your balances variable visible to users of the contract.
 mapping(address => uint) public balance_list;
 // function getBalance(address user) public view returns (uint) {
@@ -42,9 +41,9 @@ mapping(address => uint) public balance_list;
 
 // 3. Now change the constructor, to give all of the total supply to the owner of the contract.
 constructor() {
-    supply = 10000;
+    totalSupply = 10000;
     owner = msg.sender;
-    balance_list[owner] = supply;
+    balance_list[owner] = totalSupply;
 } 
 
 // 4. Now add a public function called transfer to allow a user to transfer their tokens toanother address.
@@ -52,13 +51,7 @@ constructor() {
 // the amount to transfer and
 // the recipient address.
 // 5. Add an event to the transfer function to indicate that a transfer has taken place, itshould record the amount and the recipient address.
-function transfer (uint _quantity, address _recipient) public {
-    emit balance_list[_recipient];
-}
-// 4. a) Why do we not need the sender’s address here ?
-// because the sender will be initiating the transaction and can only spend his tokens
-// 4. b) What would be the implication of having the sender’s address as a parameter?
-// the sender will be able to spend tokens that do not belong to him
+event Transferred(address to, uint amount);
 
 // 6. We want to keep a record for each user’s transfers. Create a struct called Payment that can be used to hold the transfer amount and the recipient’s address.
 struct Payment {
@@ -66,20 +59,36 @@ struct Payment {
     address recipient_address;
 }
 
+
+
 // 7. We want to reference a payments array to each user sending the payment. Create a mapping which returns an array of Payment structs when given this user’s address.
-mapping(address => uint) public Payment;
+mapping(address => Payment[]) public PaymentMapping;
 
 
-function totalSupply() public view returns (uint) {
-//    supply = _supply;
-    return supply;
+function transfer (uint _quantity, address _recipient) public {
+    balance_list[msg.sender] -= _quantity;
+    balance_list[_recipient] += _quantity;    
+    PaymentMapping[msg.sender].push(Payment(_quantity, _recipient));
+    emit Transferred(_recipient, _quantity);
 }
+// 4. a) Why do we not need the sender’s address here ?
+// because the sender will be initiating the transaction and can only spend his tokens
+// 4. b) What would be the implication of having the sender’s address as a parameter?
+// the sender will be able to spend tokens that do not belong to him
+
+
+
+function AllTotalSupply() public view returns (uint) {
+//    supply = _supply;
+    return totalSupply;
+}
+
 
 //Make a function that can increase the total supply. Inside the function, add 1000 tokensto the current total supply.
 //Make your increase total supply function public, but add your modifier so that only theowner can execute it.
 function increaseSupply(uint _amount) public onlyOwner {
-    supply = supply + _amount;
-    emit SupplyChange(supply);
+    totalSupply = totalSupply + _amount;
+    emit SupplyChange(totalSupply);
 }
 
 
